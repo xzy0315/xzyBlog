ac0900fc732f56d0175db50fbd6ad00f6bba6fda
xiongzhaoyang <xiongzhaoyang@infinibase.cn>
2017-02-24 11:46:19 +0800

    新增模糊检索功能

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ var express = require('express');
 var path = require('path');
 //处理收藏夹图标
 var favicon = require('serve-favicon');
-//写日志的
+//日志
 var logger = require('morgan');
-//解析cookier的   req.cookie方法用来设置cookie    req。cookies
+//解析cookier的   req.cookie方法用来设置cookie    req.cookies获取所有cookie
 var cookieParser = require('cookie-parser');
 //解析请求体 req.body
 var bodyParser = require('body-parser');
-//根据请求的路径不同，进行不同的处理
+//根据请求的路径不同，进行不同的处理，用于将路由细分成多个文件
 var index = require('./routes/index');
 var users = require('./routes/users');
 var articles = require('./routes/articles');
 //添加 req.session
 var session = require('express-session');
+//将session信息存储到mongo数据库中
 var MongoStore = require('connect-mongo/es5')(session);
-
+//用于设置session的success和error，仅仅用于传递参数，用于页面成功或失败的展示
 var flash = require('connect-flash');
 
 var app = express();
@@ -26,9 +27,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 //设置一下对于html格式的文件，渲染的时候委托ejs的渲染
 app.engine('html',require('ejs').renderFile);
+
 //启动是连接数据库
 var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://127.0.0.1:27017/xzyMongo');
+//连接数据库，使session保存在数据库中
 app.use(session({
     secret:'xzyBlog',
     resave:false,
@@ -36,18 +39,20 @@ app.use(session({
     store: new MongoStore({ mongooseConnection: mongoose.connection })
 }));
 
-app.use(flash());//依赖session
+app.use(flash());//依赖session，需在session后面
 
 // uncomment after placing your favicon in /public
 //需要把收藏夹的图标文件放在public下面。就可以去下下面的注释
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-//解析请求体
+//解析请求体，在req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+//解析cookie
 app.use(cookieParser());
 //静态文件服务中间件
 app.use(express.static(path.join(__dirname, 'public')));
+//自定义中间件
 app.use(function(req,res,next){
    //res.locals 才是真正的渲染模板对象
    res.locals.user = req.session.user;
@@ -56,7 +61,7 @@ app.use(function(req,res,next){
    next();
 });
 
-
+//路由配置
 app.use('/', index);
 app.use('/users', users);
 app.use('/articles', articles);
--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,39 @@ router.get('/', function(req, res, next) {
 
 });
 
+router.post('/search', function(req, res, next) {
+    //第二个参数对象最后会合并到res.locals对象上，并渲染模板
+    //先配置参数，然后再执行查询
+      console.log(req.body.searchInfo);
+    if(!req.body.searchInfo){
+        return res.redirect('back');
+    }
+    var r = new RegExp(req.body.searchInfo);
+    articleModel.find({title:r}).populate('user').exec(function(err,articles){
+        if(err){
+            req.flash('error','没有查询到响应结果');
+            res.redirect('back');
+        }else{
+            if(articles.length != 0){
+                req.flash('success','查询成功');
+                articles.forEach(function(article){
+                    article.content = markdown.toHTML(article.content);
+                });
+                res.render('index',{articles:articles,user: req.session.user});
+            }else{
+                req.flash('error','没有查询到响应结果');
+                res.redirect('/');
+            }
+        }
+    });
+
+});
+
+router.get('/search', function(req, res, next) {
+    //第二个参数对象最后会合并到res.locals对象上，并渲染模板
+    //先配置参数，然后再执行查询
+     res.redirect('/');
+
+});
+
 module.exports = router;
